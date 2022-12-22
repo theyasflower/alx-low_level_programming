7ede46a4f3424dddb04b41bbe2f91bf527f4df2a
theyasflower <84896487+theyasflower@users.noreply.github.com>
2022-12-22 05:55:23 +0100

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - prints a[2] = 98
- *
- * Return: always 0
- */
 int main(void)
 {
-    int a[5] = {0, 1, 98, 3, 4};
-    int *p;
+  int n;
+  int a[5];
+  int *p;
 
-    p = &a[0];
-    printf("a[2] = %d\n", *(p + 2));  // Add this line to print a[2] = 98 followed by a new line
-    return (0);
-}
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+ p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}                   

64381e7c5b941a8e3f0d2a74144bb9b2b2eb5aea
theyasflower <84896487+theyasflower@users.noreply.github.com>
2023-01-06 19:39:43 +0100

    Task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -6,5 +6,4 @@ ar -rc liball.a *.c
 # Create the index for the static library
 ranlib liball.a
 
-# Compile all .c files in the current directory, treating all warning messages as errors, and stopping after the compilation stage (i.e., not linking the object files into an executable)
 gcc -c *.c

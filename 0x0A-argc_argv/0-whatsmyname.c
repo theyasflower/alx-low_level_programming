b27fef8db0a3dc1efd3d96bcde1068ea1c820e47
theyasflower <84896487+theyasflower@users.noreply.github.com>
2023-01-06 20:59:29 +0100

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Print the name of the program, followed by a newline
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}

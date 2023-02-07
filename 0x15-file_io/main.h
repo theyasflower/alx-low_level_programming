69a6b9f39ff52a4218ff8d84f617ac1e9e1acd18
theyasflower <84896487+theyasflower@users.noreply.github.com>
2023-02-07 23:19:28 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,15 @@
-#ifndef IO_
-#define IO_
-int _putchar(char c);
-int _strlen(const char *s);
-ssize_t read_textfile(const char *filename, size_t letters);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letter);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-#endif
+
+#endif /* ifndef MAIN_H */
